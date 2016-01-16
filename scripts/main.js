59c5a8c0b6cc94d14434574fa19e542c4d88f6c6
thomas <thomas@mangrove.nl>
2016-01-16 19:45:53 +0100

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,6 @@ var Navigation = ReactRouter.Navigation;
 
 var History = ReactRouter.History;
 
-
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 //for using push state
 
@@ -18,8 +17,20 @@ var createBrowserHistory = require('history/lib/createBrowserHistory');
 var h = require('./helpers');
 /*include helpers js*/
 
+
+//Firebase setup
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://crackling-torch-8484.firebaseio.com/');
+
+
+/* react catalyst for bi-directional data flow*/
+var Catalyst = require('react-catalyst');
+
 /* App component */
 var App = React.createClass({
+  //making the linkedState mixin available
+  mixins : [Catalyst.LinkedStateMixin],
+  
   //sets the initial state objects which will be populated
   getInitialState: function(){
     return {
@@ -27,8 +38,32 @@ var App = React.createClass({
       order: {}
     };
   },
+  componentDidMount : function(){
+    //syncing data with firebase --> check react docs for mounts etc lifecycle
+    //runs on page load
+    base.syncState(this.props.params.storeId + '/fishes', {
+      context : this,
+      state : 'fishes'
+    });
+
+    var localstorageRef = localStorage.getItem('order-' + this.props.params.storeId);
+
+    if(localstorageRef) {
+      this.setState({
+        order : JSON.parse(localstorageRef)
+      });
+    }
+  },
+  componentWillUpdate : function(nextProps, nextState){
+    //storing in localstorage with JSON
+    localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order)); 
+
+
+  },
+  //add to the order state
   addToOrder : function(key) {
     this.state.order[key] = this.state.order[key] + 1 || 1;
+    //adding fish to order ( + 1 if same fish else = 1)
     this.setState({ order: this.state.order });
   },
   //adding the fish to the app state
@@ -40,6 +75,14 @@ var App = React.createClass({
     //set the state ( only for specific object --> performance reasons)
     this.setState({ fishes: this.state.fishes } );
   },
+  removeFish : function(key){
+    if(confirm("Are you sure?")){
+      this.state.fishes[key] = null;
+      this.setState({
+        fishes : this.state.fishes
+      });
+    }
+  },
   loadSamples : function() {
     this.setState({
       fishes: require("./sample-fishes")
@@ -47,6 +90,7 @@ var App = React.createClass({
   },
   renderFish : function(key) {
     return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>;
+    // add the fish component with the unique key -> key={key}
   },
   render: function() {
     return (
@@ -58,8 +102,8 @@ var App = React.createClass({
           {/* iteration over an array with map*/}
           </ul>
         </div>
-        <Order/>
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState} removeFish={this.removeFish}/>
         {/*pass the addfish method to inventory component*/}
       </div>
     );
@@ -68,16 +112,18 @@ var App = React.createClass({
 
 
 /*Fish*/
-
 var Fish = React.createClass({
   onButtonClick : function() {
     console.log("going to add the fish", this.props.index);
     var key = this.props.index;
     this.props.addToOrder(key);
+    // the addToOrder method is in the app component ( for setting state)
+    // and is passed down to the fish components via props
   },
   render : function() {
     var details = this.props.details;
     var isAvailable = (details.status === 'available' ? true : false);
+
     var buttonText = (isAvailable ? 'Add to order' : 'Sold out!');
     return (
       <li className="menu-fish">
@@ -89,10 +135,9 @@ var Fish = React.createClass({
         <p>{details.desc}</p>
         <button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
       </li>
-    )
+    );
   }
 });
-
 
 /* Add fish form */
 var AddFishForm = React.createClass({
@@ -134,7 +179,6 @@ var AddFishForm = React.createClass({
 /* header */
 var Header = React.createClass({
   render : function() {
-    console.log(this.props.tagline);
     return (
       <header className="top">
         <h1>Catch 
@@ -154,19 +198,81 @@ var Header = React.createClass({
 
 /* Order */
 var Order = React.createClass({
-  render : function() {
+  renderOrder : function(key){
+    var fish = this.props.fishes[key];
+    var count = this.props.order[key];
+
+    if(!fish){
+      return <li key={key}>Sorry no fiiisshhh</li>
+    }
+
     return (
-      <p>Order</p>
+      <li key={key}>
+        <span>{count}</span>lbs
+        {fish.name}
+        <span className="price">{h.formatPrice(count * fish.price)}</span>
+      </li>
+    )
+  },
+  render : function() {
+    var orderIds = Object.keys(this.props.order);
+    var total = orderIds.reduce((prevTotal, key)=> {
+      //using js reduce method to apply this to 
+      var fish = this.props.fishes[key];
+      var count = this.props.order[key];
+      var isAvailable = fish && fish.status === 'available';
+
+      if(fish && isAvailable) {
+        return prevTotal + (count * parseInt(fish.price) || 0);
+      }
+
+      return prevTotal;
+    }, 0);
+
+    return (
+      <div className="order-wrap">
+      <h2 className="order-title">Your order</h2>
+      <ul className="order">
+        {orderIds.map(this.renderOrder)}
+        <li className="total">
+          <strong>Total: </strong>
+          {h.formatPrice(total) }
+        </li>
+      </ul>
+      </div>
     );
   }
 });
 
 /* Inventory */
 var Inventory = React.createClass({
-  render : function() {
+  renderInventory : function(key){
+    var linkState = this.props.linkState;
     return (
+      <div className="fish-edit" key={key}>
+        <input type="text" valueLink={linkState('fishes.' + key + '.name')} />
+        <input type="text" valueLink={linkState('fishes.' + key + '.price')} />
+        
+        <select valueLink={linkState('fishes.' + key + '.status')}>
+          {/* using valuelink for bi directional data flow */}
+          <option value="unavailable">Sold out!</option>
+          <option value="available">Fresh</option>
+        </select>
+
+        <textarea valueLink={linkState('fishes.' + key + '.desc')}></textarea>
+        <input type="text" valueLink={linkState('fishes.' + key + '.image')}/>
+        <button onClick={this.props.removeFish.bind(null, key)}>Remove the fish jo</button>
+
+      </div>
+    );
+  },
+  render : function() {
+    return ( 
       <div>
         <h2>Inventory</h2>
+
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
+
         <AddFishForm {...this.props} />
         {/*the ... (spread) adds all the props from inventory to fishform, passes them down*/}
         <button onClick={this.props.loadSamples}>Load the sample fishes</button>       
@@ -191,7 +297,6 @@ var StorePicker = React.createClass({
 
     console.log(storeId);
     this.history.pushState(null, '/store/' + storeId);
-
     //transition from storepicker to app
   },
   render : function(){
@@ -208,21 +313,14 @@ var StorePicker = React.createClass({
   }
 });
 
-
 /* not found component */
 var NotFound = React.createClass({
-  updateCount : function(){
-    console.log(this.refs.counter.value)
-
-
-  },
   render : function() {
     return (
       <h1>Whoops! nothing here</h1>
     );
   }
 });
-
 
 /* Routes */
 var routes = (
